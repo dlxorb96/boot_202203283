4e0d91e435015b980d73d7e680b5bcef40889c07
dlxorb96 <xorb_1@naver.com>
2022-03-29 22:29:19 +0900

    2022.03.29

--- src/main/java/com/example/dto/MemberDTO.java
+++ src/main/java/com/example/dto/MemberDTO.java
@@ -21,4 +21,80 @@ public class MemberDTO {
   // 날짜 포멧을 바꿔서 보관하기 위해
   private String uregdate1;
 
+  public MemberDTO() {
+  }
+
+  public MemberDTO(String uemail, String upw, String uname, String uphone, String urole, Date uregdate,
+      String uregdate1) {
+    this.uemail = uemail;
+    this.upw = upw;
+    this.uname = uname;
+    this.uphone = uphone;
+    this.urole = urole;
+    this.uregdate = uregdate;
+    this.uregdate1 = uregdate1;
+  }
+
+  public String getUemail() {
+    return uemail;
+  }
+
+  public void setUemail(String uemail) {
+    this.uemail = uemail;
+  }
+
+  public String getUpw() {
+    return upw;
+  }
+
+  public void setUpw(String upw) {
+    this.upw = upw;
+  }
+
+  public String getUname() {
+    return uname;
+  }
+
+  public void setUname(String uname) {
+    this.uname = uname;
+  }
+
+  public String getUphone() {
+    return uphone;
+  }
+
+  public void setUphone(String uphone) {
+    this.uphone = uphone;
+  }
+
+  public String getUrole() {
+    return urole;
+  }
+
+  public void setUrole(String urole) {
+    this.urole = urole;
+  }
+
+  public Date getUregdate() {
+    return uregdate;
+  }
+
+  public void setUregdate(Date uregdate) {
+    this.uregdate = uregdate;
+  }
+
+  public String getUregdate1() {
+    return uregdate1;
+  }
+
+  public void setUregdate1(String uregdate1) {
+    this.uregdate1 = uregdate1;
+  }
+
+  @Override
+  public String toString() {
+    return "MemberDTO [uemail=" + uemail + ", uname=" + uname + ", uphone=" + uphone + ", upw=" + upw + ", uregdate="
+        + uregdate + ", uregdate1=" + uregdate1 + ", urole=" + urole + "]";
+  }
+
 }
